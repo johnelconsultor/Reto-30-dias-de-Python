d43cced40531278b06437b7d93d1bc3ae248de43
John Arias <johnelconsultor@gmail.com>
2022-04-18 16:46:56 -0500

    Update Dia8-ListasSlicing.py

--- Dia8-ListasSlicing.py
+++ Dia8-ListasSlicing.py
@@ -2,9 +2,7 @@
 #Aprende PYTHON desde el Telefóno
 #APP Gratuita en mi descripción de TK
 
-#DIA 8LISTS: Son una estructura de datos que respeta el orden de los objetos que almacenamos en ellas, permite cambiar ese orden, modificarla  y almacenae duplicados
-
-
+#DIALISTS: Son una estructura de datos que respeta el orden de los objetos que almacenamos en ellas, permite cambiar ese orden, modificarla  y almacenae duplicados
 #Seleccionar partes de una lista
 #Sistema de indices
 
@@ -15,10 +13,19 @@
 
 #Seleccionando secciones
 #En python podemos seleccionar secciones de strings creando substrings  y esto se realiza a traves del sistema de indices
-# string[#indiceInicio : #indiceFinal]
+#string[#indiceInicio:#indiceFinal]
+#print("\n SLICING con listas")
+#listaEjemplo = ["perro", "gato", "vaca", "león"]
+#domesticos1 = listaEjemplo[]
+#print(domesticos1)
+#domesticos2 = listaEjemplo[]
+#print(domesticos2)
 
-
-
+#salvajes1 = listaEjemplo[]
+#print(salvajes1)
+#salvajes2 = listaEjemplo[]
+#print(salvajes2)
+print("hello")
 
 
 
