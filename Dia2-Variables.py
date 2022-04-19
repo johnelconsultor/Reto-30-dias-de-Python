f25a3f9fe5eb786710b1fa84e349ef75dca8c2b7
John Arias <johnelconsultor@gmail.com>
2022-04-18 19:17:19 -0500

    listo

--- Dia2-Variables.py
+++ Dia2-Variables.py
@@ -3,3 +3,17 @@
 #APP Gratuita en mi descripción de TK
 
 #DIA 2 Variables
+#Son un modo de almacenar la información en python
+#El proceso de crear una variable se llama declaración
+#Para declarar:
+#nombreVariable = Número o Texto
+
+#Uso1 declarando una variable con un string
+nombre = "john"
+print("Mi nombre es: " + nombre)
+
+#Uso2 declarando una variable con un número
+numUno = 10
+numDos = 20
+#print("la suma es: ", numUno + NumDos) ACÁ ORINGINA ERROR PORQUE NO PUEDO MEZCLAR TIPOS, debo Castear Int a Str (de número a texto)
+print("la suma es: ", str(numUno + numDos))
