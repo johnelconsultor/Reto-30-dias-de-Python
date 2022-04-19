93b8e49cd49de18a67ae143d6ed0c032a04a4740
John Arias <johnelconsultor@gmail.com>
2022-04-19 13:19:03 -0500

    listo

---
+++ Dia9-ModiciarListasAgregar.py
@@ -0,0 +1,43 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 9 - Modificando listas
+#Las listas son estructuras de datos mutables cuyo orden puede ser alterado
+# animales = ["perro", "gato", "vaca", "león", "mandril"]
+# print(f"lista inicial: {animales}")
+
+# print("\n modificar lista")
+#Cambiar animales salvajes por domésticos
+# animales[-1] = "pollo"
+# animales[-2] = "burro"
+# print(f"lista modificada: {animales}")
+
+# print("\n Ver si hay algo en la lista")
+# existe = "pollo" in animales
+# print(existe)
+# noExiste = "león" in animales
+# print(noExiste)
+
+# print("\n Generando una copia de la lista")
+# animales2 = animales.copy()
+# print(animales)
+# print(animales2)
+
+# print("\n Agregar items a una lista")
+#Agrega items al final de la lista
+# animales2.append("loro")
+# print(animales2)
+# animales2.append("gallina")
+# print(animales2)
+
+# print("\n Insertar items a una lista")
+#Agrega items en una posición específica moviendo todos los items a la derecha
+# animales2.insert(2,"toro")
+# print(animales2)
+# animales2.insert(2,"gata")
+# print(animales2)
+
+#¿Se podrían preguntar, cómo sacar los items que no quiero?
+
+
---
+++ Dia9-ModiciarListasSacar.py
@@ -0,0 +1,47 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 9 - Modificando listas
+#Las listas son estructuras de datos mutables cuyo orden puede ser alterado
+# animales = ['perro', 'gato', 'gata', 'toro', 'vaca', 'burro', 'pollo', 'loro', 'gallina']
+# print(f"lista inicial: {animales}")
+
+# print("\n Remover items")
+#Para sacar items específicos de una lista
+# animales.remove("pollo")
+# animales.remove("loro")
+# animales.remove("gallina")
+# print(animales)
+
+
+# print("\n Remover items usando el método pop")
+#remueve el último item por default o alguno que específiquemos
+# animales.pop()
+# print(animales)
+# animales.pop(0)
+# print(animales)
+
+# print("\n Remover items usando la palabra clave del")
+#La palabra clave del remueve items con índice especificado, rangos o toda la lista
+# animales1 = animales.copy()
+# animales2 = animales.copy()
+# animales3 = animales.copy()
+
+# del animales1[-1] #Borra el último item
+# del animales2[-3:] #Borra desde el índice -3 al final
+# del animales3 #Borra la lista completa y hace que aparezca el error "lista no definida"
+
+# print(animales1)
+# print(animales2)
+#print(animales3)
+
+# print("\n Remover items usando el método clear")
+#Vacía la lista completamente pero no la borra como la palabra clave del
+# animales4 = animales.copy()
+# animales4.clear()
+
+# print(f"Lista semi-original {animales}")
+# print(f"Lista vacia {animales4}")
+
+# #Se preguntarán ustedes qué más se puede hacer, pués con las listas y con todas las estructuras de datos en python en general hay un montón de cosas si bien veremos métodos adicionales les recuerdo que usando la documentación encuenrtran todo más fácil
