c30065136b6153cbb63f73073e88b02af0748ab2
John Arias <johnelconsultor@gmail.com>
2022-04-19 19:31:27 -0500

    listo

---
+++ Dia9-ModListas+Métod.py
@@ -0,0 +1,31 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 9 - Otros métodos de las listas
+#Las listas son estructuras de datos mutables cuyo orden puede ser alterado
+# animales = ['perro', 'gato', 'gata', 'toro', 'vaca']
+# animales2 = ['burro', 'pollo', 'loro', 'gallina']
+
+# print("\n Símbolo +")
+#Para para unir listas
+# animalesTodos = animales + animales2
+# print(animalesTodos)
+
+# print("\n Método .extend()")
+#Para unir listas
+# animalesTodos2 = animales.extend(animales2)
+
+# print("\n Método .count()")
+#Para contar los items en una lista
+#print(animalesTodos.count())
+
+# print("\n Función Len()")
+#Para medir el largo de una lista
+#print(len(animalesTodos))
+
+# print("\n Método .Index()")
+#Para Encontrar el índice de un item específico
+#print(len(animalesTodos))
+
+#Acabamos de ver algunos de los tantos métodos adicionales pero aún nos faltan dos my importantes usados para cambiar el orden, éstos los veremos en el siguiente tiktok
---
+++ Dia9-ModListasOrden.py
@@ -0,0 +1,38 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 9 - Agregar items a listas
+#Las listas son estructuras de datos mutables cuyo orden puede ser alterado
+letras = ["b", "d", "a", "c", "e"]
+numeros = [4,1,2,5,3,0,6]
+print(letras)
+
+#print("\n Modos para ordenar las listas")
+#Existen dos modosos que podmeos usar que son:
+
+#print("\n Método .sort()")
+#Reordena los items de la lista en orden ascendente y modifica la lista original, si se le añade el argumento reverse 0 True arregla la lista en orden descendente
+# print(letras)
+# letras.sort()
+# print(letras)
+# letras.sort(reverse=True)
+# print(letras)
+
+#print("\n función sorted()")
+#Devuelve la lista ordenada sin necesidad de modificar la original
+# print(numeros)
+# print(sorted(numeros))
+# print(sorted(numeros, reverse=True))
+
+
+#print("\n .reverse para reversar las listas")
+# letrasOrdenadas = sorted(letras)
+# print(letrasOrdenadas)
+# letrasOrdenadas.reverse()
+# print(letrasOrdenadas)
+
+# numerosOrdenados = sorted(numeros)
+# print(numerosOrdenados)
+# numerosOrdenados.reverse()
+# print(numerosOrdenados)
