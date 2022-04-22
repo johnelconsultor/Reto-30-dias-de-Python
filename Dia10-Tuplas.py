a321ed61c9dd63bd23ce590077e0e7b71fedbb17
John Arias <johnelconsultor@gmail.com>
2022-04-22 15:03:43 -0500

    listos

--- Dia10-Tuplas.py
+++ Dia10-Tuplas.py
@@ -19,17 +19,17 @@
 #tuplaVacia.add("perro")
 
 #print("\n Tupla con cosas")
-frutas = ("pera", "mango", "limon", "manzana")
+# frutas = ("pera", "mango", "limon", "manzana")
 
 #Seleccionar partes de una tupla
 #Sistema de indices
 
-#print("\n Sistema de indices")
-#print("(a, \t b, \t c, \t d)")
-#print("(0, \t 1, \t 2, \t 3)")
-#print("(-4, \t -3, \t -2, \t -1)")
+# print("\n Sistema de indices")
+# print("(a, \t b, \t c, \t d)")
+# print("(0, \t 1, \t 2, \t 3)")
+# print("(-4, \t -3, \t -2, \t -1)")
 
-#print("\Cómo acceder a items específicos")
+# print("\Cómo acceder a items específicos")
 #Sintaxis tupla[#IndiceItem]
 # preferida = frutas[1]
 # preferida2 = frutas[3]
@@ -38,20 +38,19 @@ frutas = ("pera", "mango", "limon", "manzana")
 # print(f"Escogidas: {preferida}, {preferida2}, {preferida3}")
 
 
-#print("\n Cómo acceder a secciones específicas")
+# # print("\n Cómo acceder a secciones específicas")
 #Sintaxis tupla[#IndiceItemInicial : #IndiceItemFinal+1]
-# print(frutas)
+# # print(frutas)
 
-#print("Indices positivos")
+# print("Indices positivos")
 # preferidas = frutas[0:3] #La selección se corta un índice antes del final
 # preferidas2 = frutas[:3] #Si no se indica índice de inicio toma todo desde el inicio
 # print(preferidas)
 # print(preferidas2)
 
 
-#print("Indices negativos)
+print("Indices negativos")
 # noPreferidas2 = frutas[-3:] #Si no indico indice final toma todo hasta el final
-# print(noPreferidas)
-# print(noPreferidas2)
+# # print(noPreferidas2)
 
 #Al igual que las listas hay métodos que nos pueden ayudar a interactucar con las tuplas y los veremos en el siguiente tiktok
---
+++ Dia11-Sets.py
@@ -0,0 +1,40 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 11 - Sets / Conjuntos
+#La definción de sets / conjuntons en python es la misma que la de matemáticas. Un conjunto es una colección de elementos únicos sin orden específico
+#Y con ellos podemos realizar las mismas operaciones que se realizan con conjuntos
+
+#print("Crear una Set")
+#Para crear los sets utilizamos las {}
+#print("Crear un set vacio")
+#conjunto =  {}
+
+#print("Crear un set vacio con el constructor")
+#conjunto2 = set()
+
+#print("Crear un set con items")
+conjuntoF = {"cosa1","cosa2","cosa3","cosa4"}
+
+#print("\n Midiendo el largo del conjunto")
+#Utilizamos la función len() para medir el largo del conjunto
+print(conjuntoF)
+print(len(conjuntoF))
+
+
+#print("\n Accediendo a items en un conjunto")
+#print("\n Viendo si algún item existe en el conjunto")
+print("cosa2" in conjuntoF)
+print("cosa10" in conjuntoF)
+
+#print("\n Sistema de indices")
+print("{a, \t b, \t c, \t d}")
+print("{0, \t 1, \t 2, \t 3}")
+print("{-4, \t -3, \t -2, \t -1}")
+
+#print("\n Accediento a items específicos")
+
+
+
+#print("\n Sistema de indices")
