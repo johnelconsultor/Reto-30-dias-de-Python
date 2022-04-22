9a3b23e432ae90d3afc29ed53dcef9ea37898b41
John Arias <johnelconsultor@gmail.com>
2022-04-22 13:38:59 -0500

    listos

---
+++ Dia10-Tuplas.py
@@ -0,0 +1,57 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 10 - Tuplas
+#Son una colección de diferentes tipos de datos, la cual es ordenada e inmutable (no se puede cambiar)
+#Para crearlas utilizamos ()
+#Una vez creadas no podemos cambiar sus valores ni meterle o sacarle cosas porque son inmutables
+#Diferente  a las listas tiene pocos métodos
+
+#print("Crear una Tupla")
+# tuplaVacia = ()
+#Usando el constructor de tuplas
+# tuplaVacia2 = tuple()
+# print(type(tuplaVacia))
+# print(type(tuplaVacia2))
+
+#Intentemos agregar algo
+#tuplaVacia.add("perro")
+
+#print("\n Tupla con cosas")
+frutas = ("pera", "mango", "limon", "manzana")
+
+#Seleccionar partes de una tupla
+#Sistema de indices
+
+#print("\n Sistema de indices")
+#print("(a, \t b, \t c, \t d)")
+#print("(0, \t 1, \t 2, \t 3)")
+#print("(-4, \t -3, \t -2, \t -1)")
+
+#print("\Cómo acceder a items específicos")
+#Sintaxis tupla[#IndiceItem]
+# preferida = frutas[1]
+# preferida2 = frutas[3]
+# preferida3 = frutas[-1]
+
+# print(f"Escogidas: {preferida}, {preferida2}, {preferida3}")
+
+
+#print("\n Cómo acceder a secciones específicas")
+#Sintaxis tupla[#IndiceItemInicial : #IndiceItemFinal+1]
+# print(frutas)
+
+#print("Indices positivos")
+# preferidas = frutas[0:3] #La selección se corta un índice antes del final
+# preferidas2 = frutas[:3] #Si no se indica índice de inicio toma todo desde el inicio
+# print(preferidas)
+# print(preferidas2)
+
+
+#print("Indices negativos)
+# noPreferidas2 = frutas[-3:] #Si no indico indice final toma todo hasta el final
+# print(noPreferidas)
+# print(noPreferidas2)
+
+#Al igual que las listas hay métodos que nos pueden ayudar a interactucar con las tuplas y los veremos en el siguiente tiktok
---
+++ Dia10-TuplasMetodos.py
@@ -0,0 +1,44 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 10 - Métodos de tuplas
+#Son una colección de diferentes tipos de datos, la cual es ordenada e inmutable (no se puede cambiar)
+#Para crearlas utilizamos ()
+#Una vez creadas no podemos cambiar sus valores ni meterle o sacarle cosas porque son inmutables
+#Diferente  a las listas tiene pocos métodos
+
+
+#print("Cambiar tupla a lista")
+#Convertir tupla a lista
+# frutas = ("pera", "mango", "limon", "manzana")
+# print(frutas)
+# print(type(frutas))
+# print("--------------")
+# frutasLista = list(frutas)
+# print(frutasLista)
+# print(type(frutasLista))
+
+#print("\n Agregar cosas a la lista")
+# frutasLista.append("mangostino")
+# print(frutasLista)
+
+#print("\n Cambiar lista a tupla")
+#Convertir lista a tupla
+# frutasTupla = tuple(frutasLista)
+# print(frutasTupla)
+# print(type(frutasTupla))
+
+#print("\n Viendo si hay un item en la tupla")
+# print("mangostino" in frutasTupla)
+
+#print("\n Uniendo tuplas")
+#Solo se pueden unir si tienen el mismo tipo
+# vegetos = ("brocoli", "espinaca", "lechuga")
+# todo = frutasTupla + vegetos
+# print(todo)
+
+#print("\n Borrando tuplas")
+#No es posible borrar items específicos de las tuplas (inmutables) pero se puede borrar toda
+# del todo
+# print(todo)
