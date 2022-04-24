7a1d4f78246e9754abb00ef23b08dba2e4792542
John Arias <johnelconsultor@gmail.com>
2022-04-23 20:27:39 -0500

    ajustado

--- Dia11-SetsAgregarSacar.py
+++ Dia11-SetsAgregarSacar.py
@@ -7,72 +7,68 @@
 #Y con ellos podemos realizar las mismas operaciones que se realizan con conjuntos
 
 #print("Crear una Set")
-#Para crear los sets utilizamos las {}
-#print("Crear un set vacio")
-#conjunto =  {}
+#Para crear los sets utilizamos {}
+# print("Crear un set vacio")
+# conjunto = {}
 
-#print("Crear un set vacio con el constructor")
-#conjunto2 = set()
+# print("Crear un set vacio con el constructor")
+# conjunto2 = set()
+
 
 #print("Crear un set con items")
-conjuntoF = {"cosa1","cosa2","cosa3","cosa4"}
-
-#print("\n Midiendo el largo del conjunto")
-#Utilizamos la función len() para medir el largo del conjunto
-print(conjuntoF)
-print(len(conjuntoF))
+#conjuntoF = {"cosa1","cosa2","cosa3","cosa4"}
 
 #print("\n Chequeando si hay un item en un set")
 #Utilizamos el operador "in" para buscar un item dentro del set
-print(conjuntoF)
-print("cosa2" in conjuntoF)
+# print(conjuntoF)
+# print("cosa2" in conjuntoF)
 
 #print("\n Añadiendo items a un set")
 #print("Utilizando el método .add()")
 #Utilizamos el método .add() para añadir items uno por uno
-conjuntoF.add("cosa5")
-print(conjuntoF)
+# conjuntoF.add("cosa5")
+# print(conjuntoF)
 
 #print("\n Utilizando el método .update()")
 #Utilizamos el método .update() para añadir varios items a la vez, acá el argumento será una lista
-conjuntoF.update(["cosa6", "cosa7", "cosa8"])
-print(conjuntoF)
+# conjuntoF.update(["cosa6", "cosa7", "cosa8"])
+# print(conjuntoF)
 
 #print("\n Sacando items del set")
 #print("Utilizando el método .remove()")
 #Utilizamos el método .remove() para sacar items (Si el item no exite lanza error así que es mejor verificar previo)
-conjuntoF.remove("cosa8")
-print(conjuntoF)
+# conjuntoF.remove("cosa8")
+# print(conjuntoF)
 
 #print("\n Utilizando el método .pop()")
 #Utilizamos el métodos .pop() para sacer items aleatoriamente del set
-conjuntoF.pop()
-print(conjuntoF)
+# conjuntoF.pop()
+# print(conjuntoF)
 
 #print("\n Viendo el item que sacó el .pop()")
 #Si queremos ver lo que sacó
-itemSacado = conjuntoF.pop()
-print(conjuntoF)
-print(itemSacado)
+# itemSacado = conjuntoF.pop()
+# print(conjuntoF)
+# print(itemSacado)
 
 #print("\n Utilizando el método .copy()")
 #Utilizamos el método .copy() para crear copias del set
-conjuntoF1 = conjuntoF.copy()
-conjuntoF2 = conjuntoF.copy()
+# conjuntoF1 = conjuntoF.copy()
+# conjuntoF2 = conjuntoF.copy()
 
 #print("\n Utilizando el método .clear()")
 #Utilizamos el método .clear() para limpiar el set y dejarlo vacio
-conjuntoF1.clear()
-print(conjuntoF1)
+# conjuntoF1.clear()
+# print(conjuntoF1)
 
 #print("\n Utilizando la palabra clave del")
 #Utilizamos la palabra del
-del conjuntoF2
+# del conjuntoF2
 #print(conjuntoF2) #Tener cuidado porque acá nos daría error si intentamos ejecutar porque por el "del ya no existe"
 
 
-lista = list(conjuntoF)
-print(lista)
+# lista = list(conjuntoF)
+# print(lista)
 
 
 #Se preguntarán ustedes ahora que se cómo agregarle cosas a los sets, cómo puedo acceder a ellas, existen dos modos y los cubriremos en el siguiente tiktok
---
+++ Dia11-SetsMétodos.py
@@ -0,0 +1,16 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#APP Gratuita en mi descripción de TK
+
+#Dia 11 - Sets / Conjuntos
+#La definción de sets / conjuntons en python es la misma que la de matemáticas. Un conjunto es una colección de elementos únicos sin orden específico
+#Y con ellos podemos realizar las mismas operaciones que se realizan con conjuntos
+
+#print("Crear un set con items")
+conjuntoF = {"cosa1","cosa2","cosa3","cosa4"}
+
+#print("\n Midiendo el largo del conjunto")
+#Utilizamos la función len() para medir el largo del conjunto
+# print(conjuntoF)
+# print(len(conjuntoF))
+
