dc734ad492fcf2b2f318a5fd0a40391ca2ab32d7
John Arias <johnelconsultor@gmail.com>
2022-04-27 18:52:02 -0500

    listo

--- Dia14-BucleWhile.py
+++ Dia14-BucleWhile.py
@@ -4,10 +4,64 @@
 #1 Link a mi web donde esta python
 #2 Lista de reproducción con el curso
 
-#Dia 14 - Bucle While
-#Los operadores de asignación son usados para asignar valores.
-#Por ejemplo tengamos en cuanto que en Python el símbolo = se usa para
-#almacenar un valor en una variable y a éste proceso se le llama
-#asignación o asignar un valor a una variable. En python existen 
-#otros modos con los cuales se puede asignar un valor a una variable. 
-#Éstos son llamados operadores de asignación y son:
+#Dia 14 - Bucles
+#En programación es común tener que programar actividades repetitivas
+#Para ello los lenguajes de programación cuentas con algo llamado "loops" o bucles
+#Y en python tenemos dos tipos de bucles ("loops") llamados: while y for
+
+# print("Bucle while")
+# #Utilizamos la palabra reservada while para hacer bucles while. Éste es usado
+# #Para ejecutar declaraciones o bloques de código repetidamente hasta que se cumpla
+# #Una condición, cuando la condición deje de cumplirse se siguen ejecutando lso bloques
+# #Siguientes fuera del while loop
+
+# print("\n Sintaxis")
+# print("""while condición verdadera:
+#             Se ejecuta éste bloque de código 1""")
+
+# print("\n Ejemplo 1")
+# contador = 0
+# while contador < 5:
+#     print(contador)
+#     contador = contador + 1 
+
+# print("\n while + condicional")
+# #podemos complementar el while con el condicional else para se ejecute un bloque
+# #de codigo adicional cuando termine de ejecutarse el while
+
+# print("\n Ejemplo 2")
+# contador = 0
+# while contador < 5:
+#     print(contador)
+#     contador = contador + 1
+# else:
+#     print("Bucle while finalizado")
+
+
+# print("\n while + break")
+# #podemos usar la palabra reservada break para interrumbir el bucle en algún momento
+
+# print("\n Ejemplo 3")
+# contador = 0
+# while contador < 10:
+#     print(contador)
+#     contador = contador + 1
+#     if contador == 7: #Le digo que cuando llegue a 7 finalice el bucle
+#         break
+# else: 
+#     print("bucle finalizado") #NO se ejecuta el bucle se interrumpió
+
+
+# print("\n while + continue")
+# #Podemos usar la plabra clave continue para saltar a la siguiente parte
+# print("\n Ejemplo 4")
+# contador = 0
+# while contador < 10:
+#     print(contador)
+#     contador = contador + 1
+#     if contador == 7: #Le digo que cuando llegue a 7 salte al a siguiente parte
+#         continue
+# else: 
+#     print("bucle finalizado") #SE ejecuta el bucle poreu saltó acá
+
+# #Ya son unos expertos en el bucle while ahora nos falta el bucle FOR
--- Dia14-OperadoresdeAsignacion.py
+++ Dia14-OperadoresdeAsignacion.py
@@ -12,45 +12,45 @@
 #otros modos con los cuales se puede asignar un valor a una variable. 
 #Éstos son llamados operadores de asignación y son:
 
-print("Operador \t Ejemplo \t Igual a")
-print("  += \t\t x += 3 \t x = x + 3")
-print("  -= \t\t x -= 3 \t x = x - 3")
-print("  *= \t\t x *= 3 \t x = x * 3")
-print("  /= \t\t x /= 3 \t x = x / 3")
-print("  %= \t\t x %= 3 \t x = x % 3")
-print("  //= \t\t x //= 3 \t x = x // 3")
-print("  **= \t\t x **= 3 \t x = x ** 3")
+# print("Operador \t Ejemplo \t Igual a")
+# print("  += \t\t x += 3 \t x = x + 3")
+# print("  -= \t\t x -= 3 \t x = x - 3")
+# print("  *= \t\t x *= 3 \t x = x * 3")
+# print("  /= \t\t x /= 3 \t x = x / 3")
+# print("  %= \t\t x %= 3 \t x = x % 3")
+# print("  //= \t\t x //= 3 \t x = x // 3")
+# print("  **= \t\t x **= 3 \t x = x ** 3")
 
-print("\n Ejemplo1")
-#El modo más utilizado de éstos operadores es para que una variable aumente
-#con base en una acción
-print("\n Modo tradicional")
-contador = 0
-for numeros in [1,2,3,4,5]:
-    contador = contador + 5
-    print(contador)
+# print("\n Ejemplo1")
+# #El modo más utilizado de éstos operadores es para que una variable aumente
+# #con base en una acción
+# print("\n Modo tradicional")
+# contador = 0
+# for numeros in [1,2,3,4,5]:
+#     contador = contador + 5
+#     print(contador)
 
-print("\n Usando operador de asignación")
-contador = 0
-for numeros in [1,2,3,4,5]:
-    contador += 5 #contador = contador + 5 se reemplaza por contador +=5 
-    print(contador)
-#el operador de asignación me suma 5 a la variable cada iteración. El bucle
-#Itera 5 veces porque le estoy diciendo que lo haga por cada item en la lista
+# print("\n Usando operador de asignación")
+# contador = 0
+# for numeros in [1,2,3,4,5]:
+#     contador += 5 #contador = contador + 5 se reemplaza por contador +=5 
+#     print(contador)
+# #el operador de asignación me suma 5 a la variable cada iteración. El bucle
+# #Itera 5 veces porque le estoy diciendo que lo haga por cada item en la lista
 
 
-print("\n Ejemplo2")
+# print("\n Ejemplo2")
 
-print("\n Modo tradicional")
-contadorInverso = 20
-while contadorInverso > 0:
-    contadorInverso = contadorInverso - 2
-    print(contadorInverso)
+# print("\n Modo tradicional")
+# contadorInverso = 20
+# while contadorInverso > 0:
+#     contadorInverso = contadorInverso - 2
+#     print(contadorInverso)
 
-print("\n Usando operador de asignación")
-contadorInverso = 20
-while contadorInverso > 0:
-    contadorInverso -= 2
-    print(contadorInverso)
-#el operador de asignación resta 2 a la variable cada iteración. El bucle
-#Itera las veces que sea necesaria hasta que la variable deje de ser mayor a 0
+# print("\n Usando operador de asignación")
+# contadorInverso = 20
+# while contadorInverso > 0:
+#     contadorInverso -= 2
+#     print(contadorInverso)
+# #el operador de asignación resta 2 a la variable cada iteración. El bucle
+# #Itera las veces que sea necesaria hasta que la variable deje de ser mayor a 0
