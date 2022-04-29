800c21683b373e814d61bdf817fd435b47d81e46
John Arias <johnelconsultor@gmail.com>
2022-04-29 10:21:19 -0500

    listos

---
+++ Dia15-RetoBasicBMISolucion.py
@@ -0,0 +1,24 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#En mi perfil de tiktok encuentran:
+#1 Link a mi web donde esta python
+#2 Lista de reproducción con el curso
+
+#Dia 15 RETO BÁSICO VIDA EN SEMANAS
+#Generaremos una calculadora que le diga al usuario cuánta
+#vida le queda en años, meses y semanas
+#Elementos
+# Expectativa de vida: 75 años
+# Año: 365 dias, 12 meses y 52 semanas
+
+#Reto
+#Generar una calculadora del BMI que nos solicite los datos
+
+#Tareas
+#1 Generar el letrero de bienvenida
+
+#2 Generar las variables que utilizaremos y respecitvos inputs
+
+#3 Generar el cálculo del resultado y almacenarlo en una variable
+
+#4 Entregar al usuario el mensaje final con su expectativa de vida
---
+++ Dia15-RetoBasicL&WSolucion.py
@@ -0,0 +1,26 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#En mi perfil de tiktok encuentran:
+#1 Link a mi web donde esta python
+#2 Lista de reproducción con el curso
+
+#Dia 15 RETO BÁSICO CALCULADORA IMC
+#La calculadora del índice de masa corporal nos permite conocer el 
+#estado de nuestro peso con base en nuestra altura crearemos una
+#que brinde mensajes personalizados
+
+#Elementos
+#Formula IMC
+# PESO / ALTURA**2
+
+#Reto
+#Generar una calculadora del BMI que nos solicite los datos
+
+#Tareas
+#1 Generar el letrero de bienvenida
+
+#2 Generar las variables que utilizaremos y respecitvos inputs
+
+#3 Generar el cálculo del resultado y almacenarlo en una variable
+
+#4 Entregar al usuario el mensaje final con su BMI Calculado
