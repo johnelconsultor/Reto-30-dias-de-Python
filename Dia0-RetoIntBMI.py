bca927f318f2a6cd96766cf00924e7c54a1a67a6
John Arias <johnelconsultor@gmail.com>
2022-04-29 10:21:09 -0500

    listos

---
+++ Dia0-RetoIntBMI.py
@@ -0,0 +1,22 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#En mi perfil de tiktok encuentran:
+#1 Link a mi web donde esta python
+#2 Lista de reproducción con el curso
+
+#Dia 15 RETO BÁSICO CALCULADORA IMC
+#La calculadora del índice de masa corporal nos permite conocer el 
+#estado de nuestro peso con base en nuestra altura crearemos una
+#que brinde mensajes personalizados
+
+#Elementos
+#Formula IMC
+# PESO / ALTURA**2
+
+#Resultados IMC
+#Hombre: IMC < 16 DELGADEZ EXTREMA, IMC 16 Y 19 DELGADO, IMC 19 Y 25 ACEPTABLE, IMC 25 Y 35 GORDO, IMC >35 OBESO
+#Mujer: 
+
+
+#Actividad generaremos una calculadora que calcule el IMC por género y
+#que brinde un mensaje personalizado
