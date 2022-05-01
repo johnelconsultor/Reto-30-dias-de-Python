d0c891836e9ae49a56d8dd3ca0f2f4122c56f6a7
John Arias <johnelconsultor@gmail.com>
2022-05-01 10:40:11 -0500

    ajustados

--- Dia16-RetoCaculadoraPropinaSolucion.py
+++ Dia16-RetoCaculadoraPropinaSolucion.py
@@ -4,21 +4,21 @@
 #1 Link a mi web donde esta python
 #2 Lista de reproducción con el curso
 
-#Dia 15 RETO PAR O IMPAR
-#Crear un programita que permita calcular si un número
-#Ingresado es Par o Impar
+#Dia 15 RETO BÁSICO CALCULADORA PROPINA
+#Crear una calculadora de propina que pida la cuenta total
+#El % de propina, las personas involucradas y
+#Diga la propina exacta que le toca a cada una
 
 
 #Elementos
-#Recordar que un número par puede ser dividido por 2
-#Sin generar residuo
-# 3/2 : 1 y residuo de 1
+#La propina se distribuirá de manera equitativa
+#Entre los involucrados
 
 
 #Tareas
 #1 Generar el letrero de bienvenida
 
-#2 Generar las variables que utilizaremos y respecivos inputs
+#2 Generar las variables que utilizaremos y respecitvos inputs
 
 #3 Generar el cálculo del resultado y almacenarlo en una variable
 
---
+++ Dia16-RetoParoNonSolucion.py
@@ -0,0 +1,25 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#En mi perfil de tiktok encuentran:
+#1 Link a mi web donde esta python
+#2 Lista de reproducción con el curso
+
+#Dia 16 RETO PAR O IMPAR
+#Crear un programita que permita calcular si un número
+#Ingresado es Par o Impar
+
+
+#Elementos
+#Recordar que un número par puede ser dividido por 2
+#Sin generar residuo
+# 3/2 : 1 y residuo de 1
+
+
+#Tareas
+#1 Generar el letrero de bienvenida
+
+#2 Generar las variables que utilizaremos y respecivos inputs
+
+#3 Generar el cálculo del resultado y almacenarlo en una variable
+
+#4 Entregar al usuario el mensaje con el resultado final
