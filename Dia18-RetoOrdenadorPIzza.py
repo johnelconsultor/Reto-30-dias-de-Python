585d8da97cf155b5cde07e33c774c31f9b68952b
John Arias <johnelconsultor@gmail.com>
2022-05-03 14:56:45 -0500

    ajustado

---
+++ Dia18-RetoOrdenadorPIzza.py
@@ -0,0 +1,27 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#En mi perfil de tiktok encuentran:
+#1 Link a mi web donde esta python
+#2 Lista de reproducción con el curso
+
+#Dia 18 RETO Oradenador de Pizza
+#Construir un ordenador de pizza que calcule el monto total
+#Con base en los elementos dados
+
+#Elementos
+#pizzaPersonaL = 10
+#pizzaMediana = 20
+#pizzaFamiliar = 40
+
+#costo pepperoni para pequeña $1
+#costo pepperoni para las otras $3
+#Queso adicional $2
+
+#Tareas
+#1 Generar el letrero de bienvenida
+
+#2 Generar las variables que utilizaremos y respecitvos inputs
+
+#3 Generar el cálculo del resultado y almacenarlo en una variable
+
+#4 Imprimir un mensaje personalizado al usuario con el monto total
