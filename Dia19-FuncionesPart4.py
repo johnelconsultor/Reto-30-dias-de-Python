cc26b1152c2ed633202aee8e18b3092c87aaa6d5
John Arias <johnelconsultor@gmail.com>
2022-05-05 16:41:55 -0500

    listo

--- Dia19-FuncionesPart4.py
+++ Dia19-FuncionesPart4.py
@@ -10,29 +10,66 @@
 #Utilizaremos la palabra clave def. Hay que recordar que el bloque de código
 #De una función sólo será ejecutado SOLAMENTE si la función es llamada
 
-print("Declarando una función con parámetros por default")
-#Cuando creamos una función lo llamamamos "declarar" una función y cuando
-#comenzamos a usarla a ésto se le dice "llamar" o "invocar".
-#Una función puede ser declarada con o sin parámetros
+# print("Declarando una función con parámetros por default")
+# #Cuando creamos una función lo llamamamos "declarar" una función y cuando
+# #comenzamos a usarla a ésto se le dice "llamar" o "invocar".
+# #Una función puede ser declarada con o sin parámetros
 
-print("\nSintaxis de una función con parámetros por default")
-print("""def nombreDeLaFuncion ( parametro1 = valorDefault1 ):
-            bloque de código a ejecutar""")
+# print("\nSintaxis de una función con parámetros por default")
+# print("""def nombreDeLaFuncion ( parametro1 = valorDefault1 ):
+#             bloque de código a ejecutar""")
 
-print("\nEjemplo 1 de función con parámetro por default")
-def saludo (nombre = "Dataadict@"):
-    mensaje = nombre + " Que guapo eres bby ;)"
-    return mensaje
+# print("\nEjemplo 1")
+# def saludo (nombre = "Dataadict@"):
+#     mensaje = nombre + " Que guapo eres bby ;)"
+#     return mensaje
 
-print(saludo())
-print(saludo("John"))
+# print(saludo())
+# print(saludo("John"))
 
-print("\nEjemplo 2 de función con parámetro estándar")
-def pesoObjeto (masa, gravedad = 9.81):
-    peso = str( masa * gravedad) + " N"
-    return peso
+# print("\nEjemplo 2")
+# def pesoObjeto (masa, gravedad = 9.81):
+#     peso = str( masa * gravedad) + " N"
+#     return peso
 
-print(pesoObjeto(10,)) #Peso en la tierra
-print(pesoObjeto(10, 1.62)) #Gravedad en la luna = Peso en la luna
+# print(pesoObjeto(10,)) #Peso en la tierra
+# print(pesoObjeto(10, 1.62)) #Gravedad en la luna = Peso en la luna
+
+# print("\n Declarando una función con una cantidad de parámetros desconocidos")
+# #Si no conocmeos el número de argumentos que pasaremos en una función, podemos
+# #Crear una función que pueda recibir una cantidad indeterminada de argumentos
+# #Añadiendo * antes del nombre del parámetro
+# def sumarTodos(*numeros):
+#     total = 0
+#     for i in numeros:
+#         total += i
+#     return total
+
+# print(sumarTodos(1,2,3,4))
+
+# print("\n Declarando una función con parámetros default y cantidad desconocida")
+# #Podemos crear funciones que combinen todo lo que hemos visto parámetros default
+# #y parámetros con uan cantidad desconocida de argumentos
+# def generarGrupo (numGrupo, *integrantes):
+#     personasEnGrupo = []
+#     for i in integrantes:
+#         personasEnGrupo.append(i)
+#     grupo = f" El # del Grupo es{numGrupo} y los integrantes {personasEnGrupo}"
+#     return grupo
+
+# print(generarGrupo(10,"pedro","juan","john"))
 
 
+# print("\nUna función como parámetro de otra función")
+# #Las funciones pueden alimentar a otras funciones fungiendo como argumento
+# def numeroCuadrado(numero):
+#     return numero * numero
+
+# def sumaNumero(x, y):
+#     return x + y
+
+# print(sumaNumero(numeroCuadrado(10),23))
+
+
+# #Mis queridos dataadictos y dataadictas si vieron los 4 tiktokds sobre funciones
+# #Ya son todos unos expertos
