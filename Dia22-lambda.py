da8762670353e6a833c802f2621ca2c8a5186889
John Arias <johnelconsultor@gmail.com>
2022-05-19 08:56:16 -0500

    ajustado

---
+++ Dia22-lambda.py
@@ -0,0 +1,34 @@
+#JOHN EL CONSULTOR PRESENTA
+#Aprende PYTHON desde el Telefóno
+#En mi perfil de tiktok encuentran:
+#1 Link a mi web donde esta python
+#2 Lista de reproducción con el curso
+
+#Dia 22 Funciones lambda
+#Las funciones lambda son funciones pequeñas y anónimas que no tienen nombre.
+#Puden tomar cualquier cantidad de argumentos pero sólo pueden tener una expresión.
+#Para quellos que conocen javascript allí se les llaman funciones anónimas
+#Y son utilizadas para crear funciones anónimas dentro de otras funciones
+
+#Son útiles cuando:
+#Necesitas una función para un tema específico y ya
+#Necesitas una función descartable (uso una vez)
+#Cuando queremos pasar una función como argumento a una función de orden alto*
+
+print("Crear una función lambda")
+#Para crear uan función lambda usamos la palabra clave lambda
+#Seguida por los parámetros que requerirá
+#Seguida de una expresión
+print("Sintaxis de la función lambda")
+print("""x = lambda param1, param2, param3: param1 + param2 + param3
+print(x(arg1, arg2, arg3)""")
+
+print("\nEjemplo 1 Modo tradicional")
+def sumarDos():
+    return a + b
+print(sumarDos(10,20))
+
+print("\nEjemplo 1 Modo tradicional")
+def sumarDos():
+    return a + b
+print(sumarDos(10,20))
